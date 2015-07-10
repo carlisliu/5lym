77d3da6c410ca191bb26557e80e51f21575bfb1e
carlisliu <carlisliu@gmail.com>
2015-07-10 18:12:10 +0800

    support file upload

--- admin-routes.js
+++ admin-routes.js
@@ -3,10 +3,12 @@
  */
 var index = require('./routes/admin/index.js'),
     category = require('./routes/admin/category.js'),
-    article = require('./routes/admin/article.js');
+    article = require('./routes/admin/article.js'),
+    upload = require('./routes/admin/upload.js');
 
 module.exports = function (app) {
     app.use('/admin', index);
     app.use('/admin/category', category);
     app.use('/admin/article', article);
+    app.use('/admin/upload', upload);
 };
---
+++ routes/admin/upload.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Carlis on 7/10/15.
+ */
+
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+
+router.get('/', function (req, res, next) {
+    res.render('/admin/file-upload');
+});
+
+router.post('/image', function (req, res, next) {
+    //res.end('resource found');
+    var form = new formidable.IncomingForm();
+    //change temp directory.
+    form.uploadDir = "./public/upload/temp/";
+    form.parse(req, function (error, fields, files) {
+        console.error(error);
+        console.log(fields);
+        console.log(files);
+        for (var key in files) {
+            var file = files[key];
+            var fName = (new Date()).getTime();
+            switch (file.type) {
+                case "image/jpeg":
+                    fName = fName + ".jpg";
+                    break;
+                case "image/png":
+                    fName = fName + ".png";
+                    break;
+                default :
+                    fName = fName + ".png";
+                    break;
+            }
+            console.log(file.size);
+            var uploadDir = "./public/upload/" + fName;
+            fs.rename(file.path, uploadDir, function (err) {
+                if (err) {
+                    res.write(err + "\n");
+                    res.end();
+                }
+                res.write("upload image:<br/>");
+                res.write("<img src='/imgShow?id=" + fName + "' />");
+                res.end();
+            });
+        }
+    });
+});
+
+module.exports = router;
