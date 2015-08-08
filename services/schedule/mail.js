566de5765bae65f11e2cb524ac6d0f8a1e4c2a26
carlisliu <carlisliu@gmail.com>
2015-08-08 18:03:43 +0800

    add background task

--- services/schedule/mail.js
+++ services/schedule/mail.js
@@ -5,22 +5,22 @@
 var schedule = require('node-schedule');
 var nodeMailer = require('nodemailer');
 var rule = new schedule.RecurrenceRule();
-rule.hour = 15;
-rule.minute = 30;
-var j = schedule.scheduleJob(rule, function () {
+rule.hour = 9;
+rule.minute = 0;
+schedule.scheduleJob(rule, function () {
     var transporter = nodeMailer.createTransport({
         //service: 'QQ',
         port: 465,
         host: 'smtp.126.com',
         secure: true,
         auth: {
-            user: 'admin@126.com',
-            pass: 'admin'
+            user: 'light8220@126.com',
+            pass: 'woai056166'
         }
     });
 
     var mailOptions = {
-        from: 'Kris<' + 'admin@126.com' + '>', // sender address
+        from: '刘雨萌(Carlis Liu)<' + 'light8220@126.com' + '>', // sender address
         to: 'admin@sohu.com', // list of receivers
         subject: 'Hello there✔', // Subject line
         text: 'Hello world there✔', // plaintext body
