824c4487dbc2c6f6d72528f15b82b7c6cf0b1f5b
carlisliu <carlisliu@gmail.com>
2015-08-19 16:21:01 +0800

    add file

--- public/javascripts/static/admin/birthday/event.js
+++ public/javascripts/static/admin/birthday/event.js
@@ -1,3 +1,11 @@
 /**
  * Created by Carlis on 8/13/15.
  */
+define(function (require, exports, module) {
+    var $ = require('jquery');
+    var Birthday = require('./birthday');
+
+    $(function () {
+        var bDay = new Birthday();
+    });
+});
